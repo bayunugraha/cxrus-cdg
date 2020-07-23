be20f525cb6a87dda6a641668eb8986d33d10e04
baruk project <barukproject@gmail.com>
2020-07-23 12:56:10 +0700

    update

--- bayu.js
+++ bayu.js
@@ -1,10 +1,10 @@
 // owl-carousel-tabs
 $(document).ready(function () {
     $('.owl-carousel.owl-contract-scheme').owlCarousel({
-        loop: true,
+        // loop: true,
         margin: 30,
         nav: true,
-        dots: false,
+        dots: true,
         navText: [
             "<i class='fas fa-chevron-left'></i>",
             "<i class='fas fa-chevron-right'></i>"
@@ -12,6 +12,7 @@ $(document).ready(function () {
         responsive: {
             0: {
                 items: 1,
+                dots: false,
             },
             565:{
                 items: 2
@@ -19,7 +20,7 @@ $(document).ready(function () {
             767: {
                 items: 3,
             },
-            1200: {
+            1200: {  
                 items: 4
             },
             1600: {
